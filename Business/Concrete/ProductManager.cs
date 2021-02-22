ceb98e8af15cc150a87497ab5b2a1041f63eb63d
Bektaş Aydın <bektas_aydin@kocdigital.com>
2021-02-22 16:37:08 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        ICategoryDal _categoryDal;
+
+        public ProductManager(IProductDal productDal,ICategoryDal categoryDal)
+        {
+            _productDal = productDal;
+            _categoryDal = categoryDal;
+        }
+
+        public List<Product> GetAllByProduct()
+        {
+            return _productDal.GetAll();
+        }
+        public List<Category> GetAllByCategory()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice<=max);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EfProductDal(),new EfCategoryDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            foreach (var category in productManager.GetAllByCategory())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+            foreach (var product in productManager.GetAllByProduct())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+        }
+    }
+}
